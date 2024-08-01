6dc5b36e33326be4b67c4a5541e20c92b4ff215a
allkic <allkic@163.com>
2024-08-01 16:15:21 +0800

    配置：路由和国际化配置

--- locales/main.go
+++ locales/main.go
@@ -6,62 +6,29 @@ import (
 	"gopkg.in/yaml.v3"
 	"os"
 	"regexp"
-	"strings"
 )
 
 type Option map[string]interface{}
-type Locale struct {
-	zh_Hant Option
-	zh_Hans Option
-	en_US   Option
-}
+type Locale = map[string]Option
 
-var configs Locale
+var configs = Locale{}
 
-func Init() {
-	dir, _ := os.Getwd()
-	dir = strings.Replace(dir, "\\", "/", -1)
-	dir = strings.Replace(dir, "/com.lh.auth", "", -1)
-	dir = strings.Join([]string{dir, "com.lh.auth", "locales"}, "/")
-	Hans, err := os.ReadFile(dir + "/zh_Hans.yaml")
-	if err != nil {
-		configs.zh_Hans = nil
-
-	} else {
-		err = yaml.Unmarshal(Hans, &configs.zh_Hans)
+func Init(key string) {
+	dir := fmt.Sprintf("%s%s", key, "/com.lh.web.service/locales")
+	arrs := []string{"zh_Hans", "zh_Hant", "en_US"}
+	for _, val := range arrs {
+		str, err := os.ReadFile(fmt.Sprintf("%s/%s.yaml", dir, val))
 		if err != nil {
-			configs.zh_Hans = nil
+			configs[val] = Option{}
+		} else {
+			locale := Option{}
+			err = yaml.Unmarshal(str, &locale)
+			if err != nil {
+				configs[val] = Option{}
+			} else {
+				configs[val] = locale
+			}
 		}
-	}
-	Hant, err := os.ReadFile(dir + "/zh_Hant.yaml")
-	if err != nil {
-		configs.zh_Hant = nil
-
-	} else {
-		err = yaml.Unmarshal(Hant, &configs.zh_Hant)
-		if err != nil {
-			configs.zh_Hant = nil
-		}
-	}
-	ens, err := os.ReadFile(dir + "/en_US.yaml")
-	if err != nil {
-		configs.en_US = nil
-
-	} else {
-		err = yaml.Unmarshal(ens, &configs.en_US)
-		if err != nil {
-			configs.en_US = nil
-		}
-	}
-}
-
-func Get(key string) Option {
-	if key == "zh_Hant" {
-		return configs.zh_Hant
-	} else if key == "en_US" {
-		return configs.en_US
-	} else {
-		return configs.zh_Hans
 	}
 }
 
@@ -79,25 +46,20 @@ func GetLocele(c *gin.Context) string {
 }
 
 func getKeys(options Option, arrs []string) interface{} {
-	list := []interface{}{options}
-OuterLoop:
-	for _, value := range arrs {
-		item := list[len(list)-1]
-		maps, ok := item.(Option)
-		if ok {
-			vals := maps[value]
-			list = append(list, vals)
-		} else {
-			list = append(list, nil)
-			break OuterLoop
-		}
+	key := arrs[0]
+	opts := arrs[1:len(arrs)]
+	list := []interface{}{options[key]}
+	for _, val := range opts {
+		item := list[len(list)-1].(Option)
+		vals := item[val]
+		list = append(list, vals)
 	}
 	return list[len(list)-1]
 }
 
 func GetKey(c *gin.Context, arrs []string) string {
 	key := GetLocele(c)
-	locale := Get(key)
+	locale := configs[key]
 	var val string
 	val = fmt.Sprint(getKeys(locale, arrs))
 	return val
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	auth "com.lh.auth/locales"
 	"com.lh.basic/config"
+	basic "com.lh.basic/locales"
+	serve "com.lh.service/locales"
 	"com.lh.service/tools"
 	"com.lh.service/yugabyte"
-	"com.lh.web.service/locales"
+	user "com.lh.user/locales"
+	web "com.lh.web.service/locales"
 	"com.lh.web.service/router"
 	"fmt"
 	"github.com/gin-gonic/gin"
@@ -13,11 +17,16 @@ import (
 func main() {
 	app := gin.Default()
 	config.InitConfig("com.lh.web.service")
-	configs := config.GetConfig("webService")
+	root := config.GetKey("Root").(string)
+	configs := config.GetServe("webService")
 	yugabyte.InitConfig()
 	app.Use(tools.Cors())
 	//app.Use(tools.MiddleWare(configs))
-	locales.Init()
+	auth.Init(root)
+	basic.Init(root)
+	user.Init(root)
+	web.Init(root)
+	serve.Init(root)
 	router.Router(app)
 	app.Run(fmt.Sprintf("%s:%s", configs.Host, configs.Port))
 }
